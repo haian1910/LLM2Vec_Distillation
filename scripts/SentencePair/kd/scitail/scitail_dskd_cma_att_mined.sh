626ec44f7b1f4315ddce216386e5831a1441037f
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-14 10:12:45 +0700

    Update and rename yelp_dskd_cma_att_mined.sh to scitail_dskd_cma_att_mined.sh

--- scripts/SentencePair/kd/scitail/scitail_dskd_cma_att_mined.sh
+++ scripts/SentencePair/kd/scitail/scitail_dskd_cma_att_mined.sh
@@ -21,13 +21,13 @@ CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
 TEACHER_MODEL_PATH="${BASE_PATH}/model_hub/${TEACHER_MODEL_NAME}"
 # data
-DATA_DIR="${BASE_PATH}/data/yelp/"
-NUM_LABELS=5
+DATA_DIR="${BASE_PATH}/data/scitail/"
+NUM_LABELS=2
 # task
 TASK="dskd_cma_att_mined"
 # hp
 BATCH_SIZE=16
-LR=0.001
+LR=0.00001
 GRAD_ACC=1
 EVAL_BATCH_SIZE=16
 EPOCH=2
@@ -113,7 +113,7 @@ export NCCL_DEBUG=""
 export WANDB_DISABLED=True
 export TF_CPP_MIN_LOG_LEVEL=3
 export PYTHONPATH=${BASE_PATH}
-CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/Classification/distillation.py ${OPTS}"
+CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/SentencePair/distillation.py ${OPTS}"
 
 ${CMD} \
 >> ${SAVE_PATH}/train.log 2>&1 &
