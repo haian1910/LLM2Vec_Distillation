6abcaca9ba304dcd45639a8f2572aeb737adc72b
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-14 16:16:10 +0700

    Update universal_logit_distillation.py

--- Classification/criterions/universal_logit_distillation.py
+++ Classification/criterions/universal_logit_distillation.py
@@ -60,7 +60,7 @@ class UniversalLogitDistillation(CrossEntropyLoss):
         logging_output = self.record_logging_output(
             logging_output, batch_denom, log
         )
-        return loss / batch_denom, logging_output
+        return loss , logging_output
 
     def compute_universal_logit_distillation_loss(
         self, outputs, teacher_outputs, output_data, distiller, log
