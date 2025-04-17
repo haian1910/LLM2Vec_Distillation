cd2c38a9eef45cf8ed1d07839d2ab81a3f286ad1
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-17 11:35:57 +0700

    Update rmse_cka.py

--- Classification/criterions/rmse_cka.py
+++ Classification/criterions/rmse_cka.py
@@ -291,9 +291,12 @@ class RMSE_CKA(CrossEntropyLoss):
                         torch.zeros_like(student_att_for_n_token).to(device),
                         student_att_for_n_token
                     )
+                    print(loss_mse(student_att_for_n_token, teacher_att_for_n_token))
+                    
                     # Tính MSE và cộng vào att_loss_total
                     att_loss_total += loss_mse(student_att_for_n_token, teacher_att_for_n_token)
-
+                    
+            
             return att_loss_total
 
         att_loss_total_1 = compute_att_loss_1(teacher_model, model,input_data, 10) # define lại batches 
@@ -384,7 +387,7 @@ class RMSE_CKA(CrossEntropyLoss):
                     # Tính CKALoss giữa 2 ma trận
                     cka_loss = cka_loss_fn(student_att_for_k_token, teacher_att_for_k_token)
 
-                    # print("CKA Loss:", cka_loss.item())
+                    print("CKA Loss:", cka_loss.item())
                     att_loss_total  += cka_loss   
 
             return att_loss_total
