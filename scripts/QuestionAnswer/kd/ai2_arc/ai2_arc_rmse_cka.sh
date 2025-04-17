c3f21f79a97151d31f96ea6e9af548b63de290c6
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-17 23:45:08 +0700

    Update and rename scitail_rmse_cka.sh to ai2_arc_rmse_cka.sh

--- scripts/QuestionAnswer/kd/ai2_arc/ai2_arc_rmse_cka.sh
+++ scripts/QuestionAnswer/kd/ai2_arc/ai2_arc_rmse_cka.sh
@@ -21,8 +21,8 @@ CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
 TEACHER_MODEL_PATH="/content/drive/MyDrive/ColabNotebooks/data_distillation/SentencePair/Check_point_scitail/Checkpoint_teacher/criterion=cross_entropy__lora-rank=16-alpha=32-dropout=0.1-bf16__epoch=2__bsz=16x1x1=16__lr=0.00001/epoch1_step1444_loss0.1227"
 # data
-DATA_DIR="/content/drive/MyDrive/ColabNotebooks/data_distillation/SentencePair/scitail"
-NUM_LABELS=2
+DATA_DIR="/ai2_arc"
+NUM_LABELS=8
 # task
 TASK="rmse_cka"
 # hp
@@ -107,7 +107,7 @@ export NCCL_DEBUG=""
 export WANDB_DISABLED=True
 export TF_CPP_MIN_LOG_LEVEL=3
 export PYTHONPATH=${BASE_PATH}
-CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/SentencePair/distillation.py ${OPTS}"
+CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/QuestionAnswer/distillation.py ${OPTS}"
 
 ${CMD} \
 >> ${SAVE_PATH}/train.log 2>&1 &
