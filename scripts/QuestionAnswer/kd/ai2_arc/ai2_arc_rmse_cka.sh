dc96e3d1482902d71870ed5346d12e54abdbfb2a
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-18 12:44:14 +0700

    Update ai2_arc_rmse_cka.sh

--- scripts/QuestionAnswer/kd/ai2_arc/ai2_arc_rmse_cka.sh
+++ scripts/QuestionAnswer/kd/ai2_arc/ai2_arc_rmse_cka.sh
@@ -15,13 +15,13 @@ DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE \
                   --master_port $MASTER_PORT"
 
 # model
-BASE_PATH=/content/LLM2Vec_Distillation
+BASE_PATH=/LLM2Vec_Distillation
 CKPT_NAME="bert"
 CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
-TEACHER_MODEL_PATH="/content/drive/MyDrive/ColabNotebooks/data_distillation/SentencePair/Check_point_scitail/Checkpoint_teacher/criterion=cross_entropy__lora-rank=16-alpha=32-dropout=0.1-bf16__epoch=2__bsz=16x1x1=16__lr=0.00001/epoch1_step1444_loss0.1227"
+TEACHER_MODEL_PATH="link ckpt"
 # data
-DATA_DIR="/ai2_arc"
+DATA_DIR="${BASE_PATH}/data/ai2_arc/"
 NUM_LABELS=8
 # task
 TASK="rmse_cka"
