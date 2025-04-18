6941ec9633641b20ed61e010e0560a5cb66d0f37
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-18 21:37:02 +0700

    Update sst5_LLM2Vec.sh

--- scripts/Classification/sft/sst5/sst5_LLM2Vec.sh
+++ scripts/Classification/sft/sst5/sst5_LLM2Vec.sh
@@ -20,8 +20,8 @@ BASE_PATH=/LLM2Vec_Distillation
 CKPT_NAME="LLM2Vec"
 CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 # data
-DATA_DIR="${BASE_PATH}/data/patent/"
-NUM_LABELS=9
+DATA_DIR="${BASE_PATH}/data/sst5/"
+NUM_LABELS=5
 # task
 TASK="sft"
 # hp
