0529b5f69fd541d386bf458b248b3fc4af28de20
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-20 10:17:11 +0700

    Update patent_rmse_cka.sh

--- scripts/Classification/kd/patent/patent_rmse_cka.sh
+++ scripts/Classification/kd/patent/patent_rmse_cka.sh
@@ -19,7 +19,7 @@ BASE_PATH=/content/LLM2Vec_Distillation
 CKPT_NAME="bert"
 CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
-TEACHER_MODEL_PATH="/content/drive/MyDrive/ColabNotebooks/data_distillation/SentencePair/Check_point_scitail/Checkpoint_teacher/criterion=cross_entropy__lora-rank=16-alpha=32-dropout=0.1-bf16__epoch=2__bsz=16x1x1=16__lr=0.00001/epoch1_step1444_loss0.1227"
+TEACHER_MODEL_PATH="link checkpoint"
 # data
 DATA_DIR="/content/drive/MyDrive/ColabNotebooks/data_distillation/Classification/patent"
 NUM_LABELS=9
@@ -31,7 +31,7 @@ LR=0.00001
 GRAD_ACC=1
 EVAL_BATCH_SIZE=32
 EPOCH=3
-KD_RATE=0.5
+KD_RATE=0.4
 KD_TEMP=2.0
 # length
 MAX_LENGTH=512
