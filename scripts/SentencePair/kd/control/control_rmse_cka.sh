67462a6a7833f38d97c5962ccfa49f9785eaddff
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-21 14:52:32 +0700

    Update control_rmse_cka.sh

--- scripts/SentencePair/kd/control/control_rmse_cka.sh
+++ scripts/SentencePair/kd/control/control_rmse_cka.sh
@@ -19,10 +19,10 @@ BASE_PATH=/content/LLM2Vec_Distillation
 CKPT_NAME="bert"
 CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
-TEACHER_MODEL_PATH="/content/drive/MyDrive/ColabNotebooks/data_distillation/SentencePair/Check_point_scitail/Checkpoint_teacher/criterion=cross_entropy__lora-rank=16-alpha=32-dropout=0.1-bf16__epoch=2__bsz=16x1x1=16__lr=0.00001/epoch1_step1444_loss0.1227"
+TEACHER_MODEL_PATH="checkpoint"
 # data
-DATA_DIR="/content/drive/MyDrive/ColabNotebooks/data_distillation/SentencePair/scitail"
-NUM_LABELS=2
+DATA_DIR="/content/drive/MyDrive/ColabNotebooks/data_distillation/SentencePair/control"
+NUM_LABELS=3
 # task
 TASK="rmse_cka"
 # hp
