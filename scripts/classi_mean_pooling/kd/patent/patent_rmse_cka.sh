57dc6e41197adf80614b1a638ee46803195f1251
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-22 18:18:03 +0700

    Update patent_rmse_cka.sh

--- scripts/classi_mean_pooling/kd/patent/patent_rmse_cka.sh
+++ scripts/classi_mean_pooling/kd/patent/patent_rmse_cka.sh
@@ -21,7 +21,7 @@ CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
 TEACHER_MODEL_PATH="link checkpoint"
 # data
-DATA_DIR="/content/drive/MyDrive/ColabNotebooks/data_distillation/Classification/patent"
+DATA_DIR="/content/drive/MyDrive/ColabNotebooks/data_distillation/classi_mean_pooling/patent"
 NUM_LABELS=9
 # task
 TASK="rmse_cka"
@@ -100,7 +100,7 @@ export NCCL_DEBUG=""
 export WANDB_DISABLED=True
 export TF_CPP_MIN_LOG_LEVEL=3
 export PYTHONPATH=${BASE_PATH}
-CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/Classification/distillation.py ${OPTS}"
+CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/classi_mean_pooling/distillation.py ${OPTS}"
 
 ${CMD} \
 >> ${SAVE_PATH}/train.log 2>&1 &
