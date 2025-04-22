518232b0014755ee29ada9143e236cc7cf10b1a2
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-22 18:15:34 +0700

    Update patent_LLM2Vec.sh

--- scripts/classi_mean_pooling/sft/patent/patent_LLM2Vec.sh
+++ scripts/classi_mean_pooling/sft/patent/patent_LLM2Vec.sh
@@ -105,7 +105,7 @@ export NCCL_DEBUG=""
 export WANDB_DISABLED=True
 export TF_CPP_MIN_LOG_LEVEL=3
 export PYTHONPATH=${BASE_PATH}
-CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/Classification/distillation.py ${OPTS}"
+CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/classi_mean_pooling/distillation.py ${OPTS}"
 
 echo ${CMD}
 # ${CMD}
