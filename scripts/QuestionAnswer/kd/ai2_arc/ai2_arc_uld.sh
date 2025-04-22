b01c97ff1af4923aa97e0abc4f85645878966d94
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-22 22:27:45 +0700

    Update ai2_arc_uld.sh

--- scripts/QuestionAnswer/kd/ai2_arc/ai2_arc_uld.sh
+++ scripts/QuestionAnswer/kd/ai2_arc/ai2_arc_uld.sh
@@ -26,11 +26,11 @@ NUM_LABELS=8
 # task
 TASK="uld"
 # hp
-BATCH_SIZE=16
+BATCH_SIZE=4
 LR=0.00001
 GRAD_ACC=1
-EVAL_BATCH_SIZE=16
-EPOCH=2
+EVAL_BATCH_SIZE=4
+EPOCH=3
 KD_RATE=0.5
 KD_TEMP=2.0
 # length
@@ -94,14 +94,8 @@ OPTS+=" --criterion ${CRITERION}"
 OPTS+=" --seed ${SEED}"
 # deepspeed
 OPTS+=" --deepspeed"
-if [[ $PRECISION == "bf16" ]]; then
-    OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_bf16.json"
-elif [[ $PRECISION == "fp16" ]]; then
-    OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config.json"
-elif [[ $PRECISION == "fp32" ]]; then
-    OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_fp32.json"
-fi
-
+# OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_zero0.json"
+OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_test.json"
 
 export NCCL_DEBUG=""
 export WANDB_DISABLED=True
