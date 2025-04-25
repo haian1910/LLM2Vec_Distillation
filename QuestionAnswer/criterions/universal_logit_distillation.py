a4dab4b993c764dafa490cdeb96f78b18aa384ab
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-25 18:53:14 +0700

    Update universal_logit_distillation.py

--- QuestionAnswer/criterions/universal_logit_distillation.py
+++ QuestionAnswer/criterions/universal_logit_distillation.py
@@ -31,6 +31,7 @@ class UniversalLogitDistillation(CrossEntropyLoss):
         loss = self.compute_cross_entropy_loss(
             outputs.logits, output_data["labels"]
         )[0]
+        print("loss_ce": loss)
 
         # Teacher forward pass (no gradient)
         with torch.no_grad():
