21c4288b1b47aad1d4732b1145d57bd10743aa87
tmp0810 <truongminhphuc08102005@gmail.com>
2025-04-27 22:51:30 +0700

    Update medical_uld.sh

--- scripts/Classification/kd/medical/medical_uld.sh
+++ scripts/Classification/kd/medical/medical_uld.sh
@@ -21,8 +21,8 @@ CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
 TEACHER_MODEL_PATH="${BASE_PATH}/model_hub/${TEACHER_MODEL_NAME}"
 # data
-DATA_DIR="${BASE_PATH}/data/patent/"
-NUM_LABELS=9
+DATA_DIR="${BASE_PATH}/data/medical/"
+NUM_LABELS=5
 # task
 TASK="uld"
 # hp
