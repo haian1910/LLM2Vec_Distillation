5a178827e2a4551e20ba84e012a5b33c7e2ce649
haiancol <145516487+haian1910@users.noreply.github.com>
2025-04-30 06:43:43 +0700

    Update distiller.py

--- SentencePair/distiller.py
+++ SentencePair/distiller.py
@@ -253,7 +253,7 @@ class Distiller(nn.Module):
         teacher_model = PeftModel.from_pretrained(
             teacher_model, "McGill-NLP/LLM2Vec-Mistral-7B-Instruct-v2-mntp-unsup-simcse"
         )
-        # teacher_model = teacher_model.merge_and_unload()  # This can take several minutes on cpu
+        teacher_model = teacher_model.merge_and_unload()  # This can take several minutes on cpu
 
         if hasattr(self.args, 'teacher_model_path') and self.args.teacher_model_path:
             
@@ -265,16 +265,15 @@ class Distiller(nn.Module):
             fixed_checkpoint = {}
             
             for key, value in checkpoint.items():
-                # Fix the key if needed
-                new_key = key
+
                 if "lora_A.weight" in key and "default" not in key:
-                    new_key = key.replace("lora_A.weight", "lora_A.default.weight")
+                    key = key.replace("lora_A.weight", "lora_A.default.weight")
                 if "lora_B.weight" in key and "default" not in key:
-                    new_key = key.replace("lora_B.weight", "lora_B.default.weight")
+                    key = key.replace("lora_B.weight", "lora_B.default.weight")
                 if "base_model.model.base_model.model" in key:
-                    new_key = key.replace("base_model.model.base_model.model", "base_model.model")
+                    key = key.replace("base_model.model.base_model.model", "base_model.model")
                     
-                fixed_checkpoint[new_key] = value
+                fixed_checkpoint[key] = value
             
             # Save the fixed checkpoint back to the original file
             torch.save(fixed_checkpoint, adapter_path)
