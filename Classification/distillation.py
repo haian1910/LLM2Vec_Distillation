2cdb7f887a1cff7b790fee558e4ff57e6e036f4d
haiancol <145516487+haian1910@users.noreply.github.com>
2025-05-02 22:54:12 +0700

    Update distillation.py

--- Classification/distillation.py
+++ Classification/distillation.py
@@ -175,8 +175,8 @@ def finetune(args, tokenizer: AutoTokenizer, model: deepspeed.DeepSpeedEngine, o
                 if not args.only_save_projector:
                     log_rank("Saving tokenizer...")
                     tokenizer.save_pretrained(save_dir_path)
-                    # log_rank("Saving model...")
-                    # model.module.student_model.save_pretrained(save_dir_path, safe_serialization=False)
+                    log_rank("Saving model...")
+                    model.module.student_model.save_pretrained(save_dir_path, safe_serialization=False)
                     classifier_path = os.path.join(save_dir_path, "classifier_head.bin")
                     if hasattr(model.module.student_model, 'score'):  # Mistral model
                         log_rank("Saving Mistral classifier head (score)...")
