e0fc267f712ab71f40ae3bf3f12467ecc6b99deb
tmp0810 <truongminhphuc08102005@gmail.com>
2025-05-06 11:49:24 +0700

    Update tomi_dskd_cma.sh

--- scripts/SentencePair/kd/tomi/tomi_dskd_cma.sh
+++ scripts/SentencePair/kd/tomi/tomi_dskd_cma.sh
@@ -21,7 +21,7 @@ CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
 TEACHER_MODEL_PATH="${BASE_PATH}/model_hub/${TEACHER_MODEL_NAME}" # GẮN LINK MODEL CHECKPOINT VÀO ĐÂY
 # data
-DATA_DIR="${BASE_PATH}/data/scitail/"
+DATA_DIR="${BASE_PATH}/data/tomi/"
 NUM_LABELS=2
 # task
 TASK="dual_space_kd_with_cross_model_attention"
@@ -30,11 +30,11 @@ BATCH_SIZE=4
 LR=0.00001
 GRAD_ACC=1
 EVAL_BATCH_SIZE=16
-EPOCH=2
+EPOCH=3
 KD_RATE=0.5
 KD_TEMP=2.0
 # length
-MAX_LENGTH=512
+MAX_LENGTH=128
 # distiller
 PROJECTOR_CONFIG_PATH="${BASE_PATH}/configs/projector_config.json"
 PROJECTOR_LR=0.001
@@ -100,13 +100,8 @@ OPTS+=" --criterion ${CRITERION}"
 OPTS+=" --seed ${SEED}"
 # deepspeed
 OPTS+=" --deepspeed"
-if [[ $PRECISION == "bf16" ]]; then
-    OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_bf16.json"
-elif [[ $PRECISION == "fp16" ]]; then
-    OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config.json"
-elif [[ $PRECISION == "fp32" ]]; then
-    OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_fp32.json"
-fi
+OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_test.json"
+
 
 
 export NCCL_DEBUG=""
