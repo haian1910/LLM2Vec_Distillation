a8886991ff78ecf18f72aa25bbfcab9b752d2126
tmp0810 <truongminhphuc08102005@gmail.com>
2025-05-07 20:53:43 +0700

    Update SICK_LLM2Vec.sh

--- scripts/STS/sft/SICK/SICK_LLM2Vec.sh
+++ scripts/STS/sft/SICK/SICK_LLM2Vec.sh
@@ -19,8 +19,7 @@ BASE_PATH=/LLM2Vec_Distillation
 CKPT_NAME="LLM2Vec"
 CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 # data
-DATA_DIR="${BASE_PATH}/data/fomc/"
-NUM_LABELS=3
+DATA_DIR="${BASE_PATH}/data/SICK/"
 # task
 TASK="sft"
 # hp
@@ -28,15 +27,15 @@ BATCH_SIZE=2
 LR=0.00001
 GRAD_ACC=1
 EVAL_BATCH_SIZE=2
-EPOCH=2
-LORA_RANK=32
+EPOCH=5
+LORA_RANK=256
 LORA_ALPHA=16
 LORA_DROPOUT=0.1
 # length
-MAX_LENGTH=512
+MAX_LENGTH=128
 # runtime
 PRECISION="bf16"
-CRITERION="cross_entropy"
+CRITERION="sts_loss"
 CONFIG="lora-rank=${LORA_RANK}-alpha=${LORA_ALPHA}-dropout=${LORA_DROPOUT}-${PRECISION}"
 SETTING=criterion=${CRITERION}__${CONFIG}__epoch=${EPOCH}__bsz=${BATCH_SIZE}x${GRAD_ACC}x${GPUS_PER_NODE}=$((BATCH_SIZE * GRAD_ACC * GPUS_PER_NODE * NNODES))__lr=${LR}
 SAVE_PATH="${BASE_PATH}/outputs/${CKPT_NAME}/${TASK}/${SETTING}"
@@ -56,7 +55,6 @@ OPTS+=" --n-gpu ${GPUS_PER_NODE}"
 OPTS+=" --data-dir ${DATA_DIR}"
 OPTS+=" --num-workers 0"
 OPTS+=" --dev-num 1000"
-OPTS+=" --num-labels ${NUM_LABELS}"
 # task
 OPTS+=" --task ${TASK}"
 # hp
@@ -105,7 +103,7 @@ export NCCL_DEBUG=""
 export WANDB_DISABLED=True
 export TF_CPP_MIN_LOG_LEVEL=3
 export PYTHONPATH=${BASE_PATH}
-CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/Classification/distillation.py ${OPTS}"
+CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/STS/distillation.py ${OPTS}"
 
 echo ${CMD}
 # ${CMD}
